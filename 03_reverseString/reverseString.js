361202e79d7f9d54a18933bdc376fbe098967cb1
monstro1 <michael@monstro1.com>
2023-10-11 12:25:01 -0400

    Add loop to function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+	let newStr = "";
+	for (let i = 0; i < str.length; i++) {
+		newStr = str[i] + newStr;
+	}
+	return newStr;
 };
 
 // Do not edit below this line
