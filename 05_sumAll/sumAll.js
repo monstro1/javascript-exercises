7556a7126ec3649039312dc3c6ace7b6aa86e19f
monstro1 <michael@monstro1.com>
2023-10-14 11:39:10 -0400

    Add nums array

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,11 @@
 const sumAll = function(num1, num2) {
+	const nums = [...arguments];
 	if (num1 < 0 || num2 < 0) {
 		return "ERROR";
 	}
 	let sumOfNums = 0;
-	const minNum = Math.min(num1, num2);
-	const maxNum = Math.max(num1, num2);
+	const minNum = Math.min(...nums);
+	const maxNum = Math.max(...nums);
 	for (let i = minNum; i <= maxNum; i++) {
 		sumOfNums += i;
 	}
