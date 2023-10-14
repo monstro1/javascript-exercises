076d6e1e934ca2b7e94f0a9f3975c9ed3a7d4f02
monstro1 <michael@monstro1.com>
2023-10-14 11:16:10 -0400

    Add strict equality operator to if statement

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -7,7 +7,7 @@ const removeFromArray = function() {
 	let newArr = [];
 	newArrBuild: for (let i = 0; i < arr.length; i++) {
 		for (let j = 0; j < args.length; j++) {
-			if (arr[i] == args[j]) {
+			if (arr[i] === args[j]) {
 				continue newArrBuild;
 			}
 		}
