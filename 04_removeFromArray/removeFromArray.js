b15989f28e1328c044a419d10335fa6be62b2295
monstro1 <michael@monstro1.com>
2023-10-14 11:14:21 -0400

    Rewrite function to account for multiple args

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,18 @@
-const removeFromArray = function(arr, val) {
-	let newArr = [];
-	for (let i = 0; i < arr.length; i++) {
-		if (arr[i] != val) {
-			newArr.push(arr[i]);
-		}
+const removeFromArray = function() {
+	let arr = arguments[0];
+	let args = [];
+	for (let i = 1; i < arguments.length; i++) {
+		args.push(arguments[i]);
 	}
-	return newArr;
+	let newArr = [];
+	newArrBuild: for (let i = 0; i < arr.length; i++) {
+		for (let j = 0; j < args.length; j++) {
+			if (arr[i] == args[j]) {
+				continue newArrBuild;
+			}
+		}
+		newArr.push(arr[i]);
+	}
 };
 
 // Do not edit below this line
