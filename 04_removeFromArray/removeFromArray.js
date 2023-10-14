be9388c11223c7104c02e17a0d7681d7d7124330
monstro1 <michael@monstro1.com>
2023-10-14 11:01:00 -0400

    Add loop and params to function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, val) {
+	let newArr = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] != val) {
+			newArr.push(arr[i]);
+		}
+	}
+	return newArr;
 };
 
 // Do not edit below this line
