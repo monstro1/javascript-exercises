c5b0df25f3b513b1abc97ad26bbadc7138364533
monstro1 <michael@monstro1.com>
2023-10-14 11:21:37 -0400

    Add params and loop to function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,8 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+	let sumOfNums;
+	for (let i = num1; i <= num2; i++) {
+		sumOfNums += num1;
+	}
 };
 
 // Do not edit below this line
