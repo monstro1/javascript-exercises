1f319a9a706b3477b54e586c7632cb772aa58660
monstro1 <michael@monstro1.com>
2023-10-14 12:20:18 -0400

    Combine formula and rounding into return statement

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,9 @@
 const convertToCelsius = function(temp) {
-	let newTemp = (temp - 32) * (5 / 9);
-	return +newTemp.toFixed(1);
+	return +((temp - 32) * (5 / 9)).toFixed(1);
 };
 
 const convertToFahrenheit = function(temp) {
-	let newTemp = (temp * (9 / 5)) + 32;
-	return +newTemp.toFixed(1);
+	return +((temp * (9 / 5)) + 32).toFixed(1);
 };
 
 // Do not edit below this line
