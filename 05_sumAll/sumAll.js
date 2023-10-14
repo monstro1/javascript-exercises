ff6f718d47f6311bb306bb36abd0e524f849e4a4
monstro1 <michael@monstro1.com>
2023-10-14 11:32:11 -0400

    Use const for min- and maxNum vars

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,7 @@
 const sumAll = function(num1, num2) {
 	let sumOfNums = 0;
-	let minNum = Math.min(num1, num2);
-	let maxNum = Math.max(num1, num2);
+	const minNum = Math.min(num1, num2);
+	const maxNum = Math.max(num1, num2);
 	for (let i = minNum; i <= maxNum; i++) {
 		sumOfNums += i;
 	}
