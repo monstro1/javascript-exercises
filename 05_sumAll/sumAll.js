38fe13fd7817e37a8cac6a14b5de93da36e09326
monstro1 <michael@monstro1.com>
2023-10-14 11:33:28 -0400

    Add check for negative numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,7 @@
 const sumAll = function(num1, num2) {
+	if (num1 < 0 || num2 < 0) {
+		return "ERROR";
+	}
 	let sumOfNums = 0;
 	const minNum = Math.min(num1, num2);
 	const maxNum = Math.max(num1, num2);
