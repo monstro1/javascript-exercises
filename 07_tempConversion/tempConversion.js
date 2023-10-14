cd161ec60bd49f5c5bae0f256d067c1d490f28fb
monstro1 <michael@monstro1.com>
2023-10-14 12:13:27 -0400

    Add formulae to functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+	let newTemp = (temp - 32) * (5 / 9);
+	return newTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+	let newTemp = (temp * (9 / 5)) + 32;
+	return newTemp;
 };
 
 // Do not edit below this line
