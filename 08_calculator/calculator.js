99ce34b9be4f420e7536dcfa61d5e4fe7bb39e38
monstro1 <michael@monstro1.com>
2023-10-26 11:19:11 -0400

    Write sum function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,8 +6,10 @@ const subtract = function(a, b) {
 	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce(function(computedSum, currentNum) {
+		return computedSum + currentNum;
+	}, 0);
 };
 
 const multiply = function() {
