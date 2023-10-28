d496e06f4c4b1c72f716a13a2395a1f35d4e4fca
monstro1 <michael@monstro1.com>
2023-10-28 18:31:06 -0400

    Add loop and return statement

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
 const fibonacci = function(index) {
-	let sequence = [1, 1];
+	let sequence = [0, 1];
+	for (let i = 2; i < index + 1; i++) {
+		sequence.push(sequence[i - 2] + sequence[i - 1]);
+	}
+	return sequence[index];
 };
 
 // Do not edit below this line
