214897df8d0455053e722fa2ad716b8cebafe24e
monstro1 <michael@monstro1.com>
2023-10-28 18:32:56 -0400

    Add test for negative numbers

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,7 @@
 const fibonacci = function(index) {
+	if (index < 0) {
+		return "OOPS";
+	}
 	let sequence = [0, 1];
 	for (let i = 2; i < index + 1; i++) {
 		sequence.push(sequence[i - 2] + sequence[i - 1]);
