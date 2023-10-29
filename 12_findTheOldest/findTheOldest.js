023c448948338ebc3cb06f194c5de41ff6a19ce7
monstro1 <michael@monstro1.com>
2023-10-29 11:46:35 -0400

    Sort array by oldest first

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -2,6 +2,7 @@ const findTheOldest = function(arr) {
 	for (let i = 0; i < arr.length; i++) {
 		arr[i].age = arr[i].yearOfDeath - arr[i].yearOfBirth;
 	}
+	arr.sort((a, b) => b.age - a.age);
 };
 
 // Do not edit below this line
