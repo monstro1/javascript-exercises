04141358c1b250abecc15709d5cc250f92a4c3d0
monstro1 <michael@monstro1.com>
2023-10-29 11:41:46 -0400

    Add age property to objects in array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		arr[i].age = arr[i].yearOfDeath - arr[i].yearOfBirth;
+	}
 };
 
 // Do not edit below this line
